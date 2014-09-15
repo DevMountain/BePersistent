9391fbc9176d55aaf2b3382461d85eba7085e2e5
Joshua Howland <development@jkhowland.com>
2014-09-15 14:00:56 -0600

    Adds basic UI elements

--- BePersistent/AppDelegate.m
+++ BePersistent/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "PersistentViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[PersistentViewController new]];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ BePersistent/PersistentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersistentViewController.h
+//  BePersistent
+//
+//  Created by Joshua Howland on 9/15/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersistentViewController : UIViewController
+
+@end
